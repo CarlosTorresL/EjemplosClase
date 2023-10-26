32510fc0109ac0acb06272a4ce7891536bbcbe93
CarlosTorresL <97971171+CarlosTorresL@users.noreply.github.com>
2023-10-26 11:30:04 -0300

    Add files via upload

---
+++ Calc/funciones.py
@@ -0,0 +1,18 @@
+#Funciones
+
+#Función sin argumentos ni retorno
+def suma():
+    print(1+2)
+    
+#Función sin argumentos con retorno
+def resta():
+    res=2-1
+    return res
+
+#Función con argumentos sin retorno
+def mult(x,y):
+    print(x*y)
+
+#Función con argumentos con retorno
+def div(x,y):
+    return x/y
---
+++ Calc/main.py
@@ -0,0 +1,12 @@
+from funciones import *
+
+print("SUMA:")
+suma()
+print("RESTA:")
+r=resta()
+print(r)
+print("MULTIPLICACIÓN:")
+mult(5,4)
+print("DIVISIÓN:")
+res=div(5,4)
+print(res)
