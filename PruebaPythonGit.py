8c5cd4781ed409c13cfe6a7d570dd562503fe7eb
CarlosTorresL <97971171+CarlosTorresL@users.noreply.github.com>
2026-04-28 19:33:35 -0400

    Add files via upload

--- PruebaPythonGit.py
+++ PruebaPythonGit.py
@@ -1,9 +1,10 @@
 #print("Prueba de GitHub")
 print("Bienvenido")
 
-a=1
-b=3
-c=2
+a=5
+b=7
+c=1
+print("Cambio de codigo")
 #Descripción de código
 print(f"Res: {a+b*c}")
 print("Cambios Realizados")
